5e3d44f5ab5274800620e8f361faeef3a2a00cec
Oluwasegun Emmanuel Ikoya <Oluwasegun Emmanuel Ikoya>
2022-04-08 02:21:38 -0700

    Task 4 - c program to publish text using puts()

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
-/* prgram to print a string using the puts() function */
+/**
+ * main() - to print a string using the puts() function
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0); 
+	return (0);
 }
