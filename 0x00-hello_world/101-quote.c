877b25b001389e77ccb8824ad2f7e8bb17738251
Oluwasegun Emmanuel Ikoya <Oluwasegun Emmanuel Ikoya>
2022-04-08 12:44:09 -0700

    ADV TASK 2 - print to stderr without printf and puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - ptint to standard error without printf, puts and putschar
- * fwrite - takes 4 arguments, the string to be printed, the size
- * 	    of the number of data items stored,the size of each,
- * 	     the output stream.
+ * main - print to standard error without printf, 
+ * 	puts and putschar
+ * fwrite - takes 4 arguments, the string to be printed, 
+ * 	the size
+ *	of the number of data items stored,the size of each,
+ * 	the output stream.
  * Return: 1 if all goes well
 */
 
