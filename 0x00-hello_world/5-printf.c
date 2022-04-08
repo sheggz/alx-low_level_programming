35ce55bcd03743690f76e352e9966cdbbdfc3122
Oluwasegun Emmanuel Ikoya <Oluwasegun Emmanuel Ikoya>
2022-04-08 02:40:30 -0700

    Task 5- lets use printf()

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints a string using the printf() function
+ *
+ * Return: 0 if there program runs without error
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
