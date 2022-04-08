214a0f9af0c9652539a9789164f70b5adce3364e
Oluwasegun Emmanuel Ikoya <Oluwasegun Emmanuel Ikoya>
2022-04-08 12:56:17 -0700

    ADV TASK 2 - print to stderr without printf and puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -12,7 +12,6 @@
 
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19
-		", 60, 1, stderr);
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19", 60, 1, stderr);
 	return (1);
 }
