b45aec7a533778d765e41e0f19316cc4ed875bb3
Oluwasegun Emmanuel Ikoya <Oluwasegun Emmanuel Ikoya>
2022-04-08 20:22:14 -0700

    TASK 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print letter a-z
+ * Return: 0 if code runs successf'ully
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	for (i = 97; i < 123; i++)
+	{
+		if (i != 101 || i != 113)
+		{
+			putchar((char) i);
+	
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
