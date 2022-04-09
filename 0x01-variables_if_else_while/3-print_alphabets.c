0830c3f90c74dfbf8444bfa40c7241b4ebf112f8
Oluwasegun Emmanuel Ikoya <Oluwasegun Emmanuel Ikoya>
2022-04-08 20:03:29 -0700

    TASK 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print letter a-z and A-Z on same line
+ * Return: 0 if code runs successf'ully
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	for (i = 97; i < 123; i++)
+	{
+		putchar((char) i);
+	}
+	for (i = 65; i = 90; i++)
+	{
+		putchar((char) i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
