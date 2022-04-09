beca199c07cd08af3dea35c6c24d1b782695a88d
Oluwasegun Emmanuel Ikoya <Oluwasegun Emmanuel Ikoya>
2022-04-09 01:38:36 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - print the hex digits in lowercase
+ * Return: 0 if code runs successfully
+*/
+
+int main(void)
+{
+	int i = 0;
+
+	for (i = 0x30; i < 0x3A; i++)
+	{
+		putchar((char) i);
+	}
+	for (i = 0x61; i < 0x67; i++)
+	{
+		putchar((char) i);
+	}
+	putchar('\n')
+
+	return (0);
+}
