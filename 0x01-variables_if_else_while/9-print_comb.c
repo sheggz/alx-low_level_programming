eb29de81c2c3d98b316b90415b0ce58cdbc85d75
Oluwasegun Emmanuel Ikoya <Oluwasegun Emmanuel Ikoya>
2022-04-10 08:29:49 -0700

    Task 9 - implementing for loops.

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,25 +1,24 @@
 #include <stdio.h>
 /**
  * main- a program to print 0-9 with comma and space using
- * 	putchar only.
+ *	 putchar only.
  * Return: 0 if successful
 */
 
 int main(void)
 {
 	int ch;
-	
+
 	for (ch = '0'; ch < ':'; ch++) /* : is next atfer 9 on the ascii table
 					  i could've used <= 9 */
 	{
 		putchar(ch);
 		if (ch != '9')
-		{	
+		{
 			putchar(',');
 			putchar(' ');
 		}
-		putchar('\n');
 	}
-	
+	putchar('\n');
 	return (0);
 }
