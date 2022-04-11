cb87f276b6a677667f447d4dd6a3a111d0d7e814
Oluwasegun Emmanuel Ikoya <Oluwasegun Emmanuel Ikoya>
2022-04-10 23:48:16 -0700

    C - Variables, if, else, while - Adv Task 1

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - using for loops and conditional statements we print
+ * 	a combination of two numbers with no flip combination of
+ * 	same numbers and both numbers must be different
+ * Return:  if successful
+*/
+
+int main(void)
+{
+	int i,j;
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (i < j)
+			{
+				putchar(i);
+				putchar(j);
+				
+				if (i != 8 && j != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	return (0);
+}
