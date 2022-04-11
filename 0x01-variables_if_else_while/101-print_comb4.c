c2b3ddc8fe0700c3a9d62434a19d9e6dd465c155
Oluwasegun Emmanuel Ikoya <Oluwasegun Emmanuel Ikoya>
2022-04-11 15:31:18 -0700

    C - Variables, if, else, while - ADV Task 2

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -24,6 +24,7 @@ int main(void)
 					putchar(k);
 					if (i == 55 && j == 56 && k == 57)
 					{
+						putchar('\n');
 						return (0);
 					}
 					else
@@ -35,7 +36,6 @@ int main(void)
 			}
 		}
 	}
-	putchar('\n');
 
 	return (0);
 }
