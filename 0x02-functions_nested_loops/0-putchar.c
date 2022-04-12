7d248e0dad7ebca2b442a3aac16a9345b1e5dc49
Oluwasegun Emmanuel Ikoya <Oluwasegun Emmanuel Ikoya>
2022-04-12 15:22:57 -0700

    Print '_putchar'

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#include "main.h"
+#include "_putchar.c"
 /**
  * main - print '_putchar'
  * Return: 0 if successful
@@ -6,6 +7,15 @@
 
 int main(void)
 {
-	printf("_putchar");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
 	reuturn (0);
 }
