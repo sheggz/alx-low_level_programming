2769444defafb9661a4b2e7c86b8466ab42cdf72
Oluwasegun Emmanuel Ikoya <Oluwasegun Emmanuel Ikoya>
2022-04-12 16:40:54 -0700

    create a function that prints lowercase letters
    we use print using a predefined function _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - print the lowercase alphabets with a function whose prototypr
+ * 	 is in main.
+ * Return: 0 if successful
+*/
+
+void print_alphabet(void)
+{ 
+	int i;
+	
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar((char) i);	
+	}	
+	_putchar('\n');
+	return (0);
+}
