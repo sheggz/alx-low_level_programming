8250d2fa7ea6d4dd3c56fa89b68efeac3cf37cf7
Oluwasegun Emmanuel Ikoya <Oluwasegun Emmanuel Ikoya>
2022-04-12 17:44:15 -0700

    Check if argument is lowercase alphabet

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - check if parameter is lowercase or not
+ * Return: 1 if c is lowercase, 0 otherwise
+*/
+
+int _islower(int c)
+{
+	if ( c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
