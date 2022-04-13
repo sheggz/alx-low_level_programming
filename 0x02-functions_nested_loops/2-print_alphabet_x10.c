9244d62afc3cdd11e9d0fb5c3d68bcb7cf0f263a
Oluwasegun Emmanuel Ikoya <Oluwasegun Emmanuel Ikoya>
2022-04-12 17:26:08 -0700

    Print the lowercase alphabets 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - use nested for loops to print
+ *		the alphabets 10 times
+*/
+
+void print_alphabet_x10(void)
+{
+	int i,j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 97; j < 123; j++)
+		{
+		 	_putchar(j);
+		}
+	}
+	_putchar('\n');
+}
