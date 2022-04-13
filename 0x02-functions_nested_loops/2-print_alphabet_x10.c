58ceed81de44b0c2c6b60d5d2e91f1dfb5ce537c
Oluwasegun Emmanuel Ikoya <Oluwasegun Emmanuel Ikoya>
2022-04-12 17:31:58 -0700

    Print lowercase alphabets 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,14 +6,15 @@
 
 void print_alphabet_x10(void)
 {
-	int i,j;
+	int i, j;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 97; j < 123; j++)
 		{
-		 	_putchar(j);
+			_putchar(j);
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
+	
 }
