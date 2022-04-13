14825c7f1ef4d6d2af7e78e86dcda3b0034797ad
Oluwasegun Emmanuel Ikoya <Oluwasegun Emmanuel Ikoya>
2022-04-13 14:53:36 -0700

    Fix wrong o/p for program to print every min of day
    Previous output was wrong because _putchar only prints the
    character equivalent of integers. i added 48 to the result
    to make the resilting interger fall int the decimal character
    equivalent on the ASCII table

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,11 +13,12 @@ void jack_bauer(void)
 	{
 		for (j = 0; j <= 59; j++)
 		{
-			_putchar(i / 10);
-			_putchar(i % 10);
+			_putchar((i / 10) + 48);
+			_putchar((i % 10) + 48);
 			_putchar(':');
-			_putchar(j / 10);
-			_putchar(j % 10);
+			_putchar((j / 10) + 48);
+			_putchar((j % 10) + 48);
+			_putchar('\n');
 		}
 	}
 
