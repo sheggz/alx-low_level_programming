085e6a288ae0f92764722769054c2ba641636337
Oluwasegun Emmanuel Ikoya <Oluwasegun Emmanuel Ikoya>
2022-04-14 04:18:43 -0700

    Fix extra comma error and wrong o/p error for n > 99

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,27 +8,33 @@ void print_to_98(int n)
 {
 	int i;
 
-	if (n >= 98 && n < 1000)
+	if (n >= 98 && n < 10000)
 	{
 		for (i = n; i >= 98; i--)
 		{
 			if ((i / 1000) != 0)
 			{
 				_putchar((i / 1000) + '0');
-				_putchar((i % 1000) + '0');
+				_putchar(((i % 1000) / 100) + '0');
+				_putchar((((i % 1000) % 100) / 10) + '0');
+				_putchar((((i % 1000) % 100) % 10) + '0');
 			}
 			else if ((i / 100) != 0)
 			{
 				_putchar((i / 100) + '0');
-				_putchar((i % 100) + '0');
+				_putchar(((i % 100) / 10) + '0');
+				_putchar(((i % 100) % 10) + '0');
 			}
 			else if ((i / 10) != 0)
 			{
 				_putchar((i / 10) + '0');
 				_putchar((i % 10) + '0');
 			}
-			_putchar(',');
-			_putchar(' ');
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 	}
 	else if (n >= -1000 && n < 98)
@@ -40,13 +46,16 @@ void print_to_98(int n)
 				if ((-i / 1000) != 0)
 				{	_putchar('-');
 					_putchar((-i / 1000) + '0');
-					_putchar((-i % 1000) + '0');
-				}	
+					_putchar(((-i % 1000) / 100) + '0');
+					_putchar((((i % 1000) % 100) / 10) + '0');
+					_putchar((((i % 1000) % 100) % 10) + '0');
+				}
 				else if ((-i / 100) != 0)
 				{
 					_putchar('-');
 					_putchar((-i / 100) + '0');
-					_putchar((-i % 100) + '0');
+					_putchar(((-i % 100) / 10) + '0');
+					_putchar(((-i % 100) % 10) + '0');
 				}
 				else if ((-i / 10) != 0) 
 				{
@@ -62,24 +71,25 @@ void print_to_98(int n)
 				{
 					_putchar('-');
 					_putchar(-i + 48);
-				}	
+				}
 			}
-			else 
+			else
 			{
 				if ((i / 10) != 0)
 				{
 					_putchar((i / 10) + '0');
 					_putchar((i % 10) + '0');
 				}
-				else 
+				else
 				{
 					_putchar(i + 48);
 				}
-
 			}
-			_putchar(',');
-			_putchar(' ');
-	
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 
 	}
