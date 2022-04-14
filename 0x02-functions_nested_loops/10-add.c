551793f04930bfbf9f499e57b5051071da58ccd7
Oluwasegun Emmanuel Ikoya <Oluwasegun Emmanuel Ikoya>
2022-04-13 22:52:19 -0700

    Create a C function to add 2 numbers (int)

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add -  adds two integers and returns the result;
+ * @num1: first number to be added
+ * @num2: second number to be added
+ * Return: the sum of both numbers
+ */
+
+int main (int num1, int num2)
+{
+	return(num1 + num2);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * times_table - a function that prints the 9 times table, starting with 0.
+ */
+
+void times_table(void)
+{
+	int i,j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			_putchar( i * j
+		}
+	}
+}
