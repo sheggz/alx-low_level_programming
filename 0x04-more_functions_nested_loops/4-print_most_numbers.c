8e51e0db9ee7ca26883b478e24e44bbb0e575d2d
Oluwasegun Emmanuel Ikoya <Oluwasegun Emmanuel Ikoya>
2022-04-14 16:23:06 -0700

    Create a C function that prints 01356789

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_most_numbers - a function that prints the numbers, 
+ *		       from 0 to 9, followed by a new line.
+*/
+
+void print_most_numbers(void)
+{
+	int i;
+	
+	for (i = 48; i < 58; i++)
+	{
+		if (i = 50 || i = 52)
+		{
+			continue;
+		}
+		_putchar(i);
+	}
+	_putchar('\n');
+}
