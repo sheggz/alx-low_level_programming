eea1f083845c6d70f0757862a1c3c7fee91d71f0
Oluwasegun Emmanuel Ikoya <Oluwasegun Emmanuel Ikoya>
2022-04-14 15:12:42 -0700

    Create a function that prints 0 - 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - a function that prints the numbers, from 0 to 9, followed by a new line.
+ */
+
+void print_numbers (void)
+{
+	int i = 48
+
+	while (i <= 57)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
