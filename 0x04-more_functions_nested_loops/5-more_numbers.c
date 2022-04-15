c43966e3145c24b310f8d02f6b567e3fb292b4f6
Oluwasegun Emmanuel Ikoya <Oluwasegun Emmanuel Ikoya>
2022-04-14 20:42:55 -0700

    Fix muddled output

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,15 +12,12 @@ void more_numbers(void)
 	{
 		for (j = 0; j < 15; j++)
 		{
-			if ( j < 10)
-			{
-				_putchar((j % 10) + '0');
-			}
-			else
+			if ( j > 9)
 			{
 				_putchar((j / 10) + '0');
-				_putchar((j % 10) + 48);
 			}
+			_putchar((j % 10) + 48);
 		}
+		_putchar('\n');
 	}
 }
