55f63536da9a663ef5f167664cd15cd3c84f0910
Oluwasegun Emmanuel Ikoya <Oluwasegun Emmanuel Ikoya>
2022-04-15 14:24:27 -0700

    Fix wrong output for some test cases

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_square -  a function that prints a square, followed by a new line.
+ * print_square - a function that prints a square, followed by a new line.
  * @size: function variable for size of the square
  */
 
@@ -11,9 +11,12 @@ void print_square(int size)
 
 	for (i = 0; i < size; i++)
 	{
-		for(j = 0; j < size; j++)
+		if (size > 0)
 		{
-			_putchar('#');
+			for(j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
 		}
 		_putchar('\n');
 	}
