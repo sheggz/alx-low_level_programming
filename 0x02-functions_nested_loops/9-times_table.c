f2e35dc0068da7030a6ca95b946ff28fde4d6397
Oluwasegun Emmanuel Ikoya <Oluwasegun Emmanuel Ikoya>
2022-04-18 11:32:45 -0700

    Complete the 9 times table script

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,13 +6,23 @@
 
 void times_table(void)
 {
-	int i,j;
+	int i, j;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			_putchar( i * j
+			if((i * j) > 10)
+			{
+				_putchar(((i * j) / 10) + '0');
+			}
+			_putchar(((i * j) % 10) + 48);
+			if (j < 9)
+			{	
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
+		_putchar('\n');
 	}
 }
