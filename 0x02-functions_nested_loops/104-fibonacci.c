489d2202d52e2763453e53f4b81e6d5d9368dcb2
Oluwasegun Emmanuel Ikoya <Oluwasegun Emmanuel Ikoya>
2022-04-19 22:09:11 -0700

    Create a C script to print first 98 fibonacci numbers

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print first 98 numbers of fibonacci sequence
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	long int i = 1, sum = 0, n1 = 1, n2 = 2;
+
+	printf("%lu, %lu", n1, n2);
+	while (i <= 96)
+	{
+		sum = n1 + n2;
+		printf(", %lu", sum);
+		n1 = n2;
+		n2 = sum;
+		i++;
+	}
+	printf("\n");
+
+	return (0);
+}
