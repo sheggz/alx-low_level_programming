d33b81254c8dd96c8e686b2b41e1cf5407e3565e
Oluwasegun Emmanuel Ikoya <Oluwasegun Emmanuel Ikoya>
2022-04-19 21:14:44 -0700

    Fix infinite loop and wrong styling

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,13 +9,14 @@ int main(void)
 {
 	int i = 0, sum = 0, n1 = 1, n2 = 2;
 
-	printf("%d,%d", n1, n2);
+	printf("%d, %d", n1, n2);
 	while (i <= 48)
 	{
 		sum = n1 + n2;
-		printf(",%d", sum);
+		printf(", %d", sum);
 		n1 = n2;
 		n2 = sum;
+		i++;
 	}
 	printf("\n");
 
