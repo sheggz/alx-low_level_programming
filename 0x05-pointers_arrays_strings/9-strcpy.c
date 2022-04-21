c4418bc32cfbba0acc8a8c3a3f7efd69c94ef008
Oluwasegun Emmanuel Ikoya <Oluwasegun Emmanuel Ikoya>
2022-04-21 08:01:54 -0700

    Create a C script to copy a string into another array

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strcpy -  a function that copies the string pointed to by src, including 
+ *	      the terminating null byte (\0), to the buffer pointed to by dest
+ * @dest: pointer to buffer where string is copied to
+ * @src: Pointer to string to be copied
+ * Return: a pointer to a character array
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i, len = 0;
+
+	while ((*(scr + len) != '\0'))
+		++len;
+
+	for (i = 0; i < len; ++i)
+	{
+		dest[i] = src[i];
+	}
+	dest[len] = '\0';
+	return (dest);
+}
+
