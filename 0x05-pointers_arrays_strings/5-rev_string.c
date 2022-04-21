438dc7571e9b31ff0b7eb48880903e9eda1292ec
Oluwasegun Emmanuel Ikoya <Oluwasegun Emmanuel Ikoya>
2022-04-21 03:14:02 -0700

    Create a C script to reverse a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rev - a fn that prints a string in reverse
+ * @s: pointer to a string
+ */
+
+void print_rev(char *s)
+{
+	int i, len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	
+	for (i = len; i>= 0; i--)
+	{
+		_putchar(*(s + i));
+	}
+}
