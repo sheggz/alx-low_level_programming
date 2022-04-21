798a91168a9a328739801280a94c92ad9af72994
Oluwasegun Emmanuel Ikoya <Oluwasegun Emmanuel Ikoya>
2022-04-21 06:08:59 -0700

    Fix styling error

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -13,16 +13,17 @@ void rev_string(char *s)
 		len++;
 
 	for (i = len - 1; i >= len / 2; --i)
-	/* loop starts from len -1 because we must start from 
+	/*
+	 * loop starts from len -1 because we must start from
 	 * position 0. and we stop at len/2 because if len == 10
 	 * then we need 5 steps to swap all the components
 	 */
 	{
 		cache = s[i];
 		s[i] = s[j];
-		s[j++] = cache; /* j is used to run throug the array
+		s[j++] = cache; /*
+				 * j is used to run throug the array
 				 * from begining
 				 */
 	}
-
 }
