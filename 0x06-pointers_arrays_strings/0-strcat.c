13d056db49c7f1779414e5273fff3aace3fb9cc4
Oluwasegun Emmanuel Ikoya <Oluwasegun Emmanuel Ikoya>
2022-04-23 13:13:09 -0700

    Create the main file and correct wrong o/p

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,11 +1,12 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
  * _strcat - function appends the src string to the dest string, overwriting
 	     the terminating null byte (\0) at the end of dest, and then adds
 	     a terminating null byte
- * @src - pointer to string to be appended
- * @despointert to string to be appended to
+ * @src:  pointer to string to be appended
+ * @dest: pointer to string to be appended to
  * Return:  pointer to the resulting string dest
  */
 
@@ -15,13 +16,17 @@ char *_strcat(char *dest, char *src)
 
 	while (src[len_src] != '\0')
 		len_src++;
-	while (src[len_dest] != '\0')
+	while (dest[len_dest] != '\0')
 		len_dest++;		
 
 	for (i = 0; i <= len_src; ++i)
 	{
-		 dest[len_dest++] = src[i];
+		/*printf("%d, %d\n", i, len_dest);*/
+		dest[len_dest++] = src[i];
+
+		/*printf("%c, %c, %s\n", src[i], dest[len_dest], dest);*/
 	}
+	/* dest[len_dest + 1] = '\0';*/
 
 	return dest;
 
