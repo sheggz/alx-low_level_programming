741446992fc920e55c500aa7ad2844ec8c570f84
Oluwasegun Emmanuel Ikoya <Oluwasegun Emmanuel Ikoya>
2022-04-24 04:46:26 -0700

    Create a C script to concatenate 2 strings knowing the size of the Char array to be concatenated

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncat - function appends the src string to the dest string, overwriting
+	     the terminating null byte (\0) at the end of dest, and then adds
+	     a terminating null byte
+ * @src:  pointer to string to be appended
+ * @dest: pointer to string to be appended to
+ * @n: the most bytes src can use
+ * Return:  pointer to the resulting string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int len_dest = 0, i;
+
+	while (dest[len_dest] != '\0')
+		len_dest++;
+
+	for (i = 0; i <= n; ++i)
+	{
+		/* printf("%d, %d\n", i, len_dest); */
+		dest[len_dest++] = src[i];
+
+		/* printf("%c, %c, %s\n", src[i], dest[len_dest], dest); */
+	}
+	/* dest[len_dest + 1] = '\0'; */
+
+	return (dest);
+}
