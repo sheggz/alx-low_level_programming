c2576c5f1675083b039b3647a2c8e5ae73cd551d
Oluwasegun Emmanuel Ikoya <Oluwasegun Emmanuel Ikoya>
2022-04-24 14:02:53 -0700

    Fix wrong output

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -19,9 +19,9 @@ char *_strncat(char *dest, char *src, int n)
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 
-		dest[dest_len + i] = src[i];
+		dest[len_dest + i] = src[i];
 
-	dest[dest_len + i] = '\0';
+	dest[len_dest + i] = '\0';
 
 	return (dest);
 }
