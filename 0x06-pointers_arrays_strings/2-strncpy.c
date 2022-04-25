a9e3a21fdc0b4bb7e9ead5672e96485ac57b4d83
Oluwasegun Emmanuel Ikoya <Oluwasegun Emmanuel Ikoya>
2022-04-24 21:03:50 -0700

    Create a C script to copy n bytes of a string to a new location

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strncpy - a function that copies a string.
+ * @dest: pointer to Buffer where src is to be copied to 
+ * @src: string to be copied
+ * @n: max no of bytes to be copied
+ * Return: a pointer to dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+	
+	return (dest);                                                         
+}
+	
