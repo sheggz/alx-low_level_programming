c8c9b976972e3f67489cfd82d98760095305e56c
Oluwasegun Emmanuel Ikoya <Oluwasegun Emmanuel Ikoya>
2022-04-24 21:52:35 -0700

    Create a c script to compare strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,22 +1,15 @@
-
-#include "main."
+#include "main.h"
 
 /**
-
  * _strcmp - a function that compares two strings.
  * @s1: an input string
-
  * @s2: an input strin
  * Return: The difference s1 - s2
-
  */
 
 int _strcmp(char *s1, char *s2)
-
 {
-
 	while (*s1 && *s2)
-
 	{
 
 		if (*s1 != *s2)
@@ -28,5 +21,4 @@ int _strcmp(char *s1, char *s2)
 	}
 
 	return (*s1 - *s2);
-
 }
