4c95d689f244d684905aee77e669031ef065d7c9
Oluwasegun Emmanuel Ikoya <Oluwasegun Emmanuel Ikoya>
2022-04-30 05:57:32 -0700

    Fix Styling error

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,13 +10,17 @@ int _strlen_recursion(char *s)
 {
 /*
  * THE EXPLANATION - case study "CARS"; a string with 4 letters
- * 4 = 1 + 3; 4 = 1 + (1 + 2); 4 = 1 + (1 + (1 + 1)); 4 = 1 + (1 + (1 + (1+0)))
+ * 4 = 1 + 3;
+ * 4 = 1 + (1 + 2);
+ * 4 = 1 + (1 + (1 + 1));
+ * 4 = 1 + (1 + (1 + (1 + 0)))
  * we have successfuly been able to express a complex count as a repition of a
  * pattern i.e 1 + (number of letters remaining) and this is what each
  * function call should return. hence; in code we have:
+ * ALWAYS REMEMBER THAT THE I/P TO OUR FUNCTION IS AN ADDRESS
 */
-	
-	if(*s == '\0')
+
+	if (*s == '\0')
 		return (0);
 	return (1 + _strlen_recursion(&(*(s + 1))));
 }
