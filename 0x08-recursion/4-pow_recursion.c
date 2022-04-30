ec3771f3428fad031da5acc7b411abb97f6717c5
Oluwasegun Emmanuel Ikoya <Oluwasegun Emmanuel Ikoya>
2022-04-30 06:48:34 -0700

    Fix wring o/p for negative index

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -22,7 +22,7 @@ int _pow_recursion(int x, int y)
 	else if (y == 0)
 		return (1);
 	else if (y < 0)
-		return (1 / (x * _pow_recursion(x, (-y) - 1)));
+		return (-1);
 	return (x * _pow_recursion(x, y - 1));
 
 }
