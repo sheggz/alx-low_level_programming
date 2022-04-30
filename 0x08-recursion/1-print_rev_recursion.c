8a0fe5eb15052e45fea88ffbad91950f25473106
Oluwasegun Emmanuel Ikoya <Oluwasegun Emmanuel Ikoya>
2022-04-29 22:18:52 -0700

    Create a C script to print a stringin reverse with recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - a function that prints a string in reverse
+ * @s: pointer that holds the address of the first element of char array
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0') /* can be written as if (!*s)*/
+		return;
+	_print_rev_recursion(s + 1);
+	_putchar(*s); /*
+		       * this means that it will only start printing when
+		       * it has gotten to the end of the string, i.e while 
+		       * descending the stack.
+		       */
+}
