a32a4e83d824b90259baa774a98e0af4ef746cce
Oluwasegun Emmanuel Ikoya <Oluwasegun Emmanuel Ikoya>
2022-04-30 05:49:35 -0700

    Create a c script that uses Recursion to count the no of letters in a string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,10 +8,15 @@
 
 int _strlen_recursion(char *s)
 {
-	int i = 0;
+/*
+ * THE EXPLANATION - case study "CARS"; a string with 4 letters
+ * 4 = 1 + 3; 4 = 1 + (1 + 2); 4 = 1 + (1 + (1 + 1)); 4 = 1 + (1 + (1 + (1+0)))
+ * we have successfuly been able to express a complex count as a repition of a
+ * pattern i.e 1 + (number of letters remaining) and this is what each
+ * function call should return. hence; in code we have:
+*/
 	
-	if (*s == '\0')
-		return (i);
-	++i;
-	_strlen_recursion(&(*(s + 1)));
+	if(s* == 0)
+		return (0);
+	return (1 + _strlen_recursion(&(*(s + 1))));
 }
