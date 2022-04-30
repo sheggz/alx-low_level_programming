4702f99b5e1058957359ce66351916e2c0be1adb
Oluwasegun Emmanuel Ikoya <Oluwasegun Emmanuel Ikoya>
2022-04-29 20:53:04 -0700

    Create a C script to print a string using Recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - a function that prints a string, followed by a new line
+ * @s: pointer to an input string
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		_putchar('\n');
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(&(*(s + 1))); /* or s + 1 */
+	}
+}
