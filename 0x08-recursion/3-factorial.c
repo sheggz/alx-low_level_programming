6cd02044dd63f1a9f156cf1cbbb7b65c81551cd4
Oluwasegun Emmanuel Ikoya <Oluwasegun Emmanuel Ikoya>
2022-04-30 06:09:14 -0700

    Create a C function to calculate the factorial of n

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * factorial - a function that returns the factorial of a given number.
+ *		using recursion.
+ * @n: positive integer who's factorial is to be calculated
+ * Return: the factotial of n.
+*/
+
+int factorial(int n)
+{
+	/* n! = n * (n - 1)! */
+
+	/* BASE CASE */
+	if (n == 0 || n == 1)
+		return (1);
+	return (n * factorial(n - 1));
+}
