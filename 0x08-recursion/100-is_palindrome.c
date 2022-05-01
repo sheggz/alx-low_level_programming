67c7ebf3ee25a9f71d6dbb31d6f79f10bbb1f24a
Oluwasegun Emmanuel Ikoya <Oluwasegun Emmanuel Ikoya>
2022-05-01 12:23:20 -0700

    Fix compile error

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,6 +1,6 @@
 #include "main.h"
-void fill_forward(char *w, char []);
-void fill_reverse(char *x, char []);
+void fill_forward(int *, char *w, char []);
+void fill_reverse(int *, char *x, char []);
 /**
  * is_palindrome - a function that returns 1 if a string is a palindrome
  *		   and 0 if not
@@ -15,9 +15,9 @@ int is_palindrome(char *s)
 	char forward [100];
 	char reverse [100];
 
-	fill_forward(s, forward);
+	fill_forward(&i, s, forward);
 	i = 0;
-	fill_reverse(s, reverse);
+	fill_reverse(&i, s, reverse);
 
 	if (forward == reverse)
 		return (1);
@@ -25,18 +25,18 @@ int is_palindrome(char *s)
 		return (0);
 }
 
-void fill_forward(char *w, char forward_arr [])
+void fill_forward(int *i, char *w, char forward_arr [])
 {
 	if (*w == '\0')
 		return;
-	forward_arr[i++] = *w;
-	fill_forward(w + 1, forward_arr);
+	forward_arr[*i++] = *w;
+	fill_forward(&(*i), w + 1, forward_arr);
 }
 
-void fill_reverse(char *x, char reverse_arr [])
+void fill_reverse(int *i, char *x, char reverse_arr [])
 {
 	if (*x == '\0')
 		return;
-	fill_reverse(x + 1, reverse_arr);
-	reverse_arr[i++] = *x;
+	fill_reverse(&(*i), x + 1, reverse_arr);
+	reverse_arr[*i++] = *x;
 }
