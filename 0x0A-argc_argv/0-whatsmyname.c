2c925d255cc3cd4db462a7df4a2e1fbc5b475aaa
Oluwasegun Emmanuel Ikoya <Oluwasegun Emmanuel Ikoya>
2022-05-02 13:37:43 -0700

    Create a program that prints its name with the path

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main -print the name of the program using argv
+ * @argc: argument counter
+ * @argv: argument vector - array of all arguments used to run the program
+ * Return: ) if successful
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("./%s\n", argv[0]);
+	return (0);
+}
