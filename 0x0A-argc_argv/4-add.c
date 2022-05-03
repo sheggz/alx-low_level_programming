76048a58b91dd678ce41707ee2f1cc603cd71068
Oluwasegun Emmanuel Ikoya <Oluwasegun Emmanuel Ikoya>
2022-05-02 17:51:06 -0700

    Create a c program to add numbers from cmd line

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - add 2 numbers supplied during program call in the cmd line
+ * @argc: argument counter
+ * @argv: argument vector - array of all arguments used to run the program
+ * Return: ) if successful
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, result = 0;
+
+	if (argc == 1) /* if no number is supplied */
+	{
+		printf("0\n");
+		return (1);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (argv[i] > 57 || argv[i] < 48)
+			{
+				printf("Error\n");
+				return (1);
+			}
+			result = result + argv[i];
+		}
+	}
+	printf("%d\n", result);
+	return (0);
+}
