b6551e1d8913a1330bf39e49f14e4fc233f39039
Oluwasegun Emmanuel Ikoya <Oluwasegun Emmanuel Ikoya>
2022-05-04 16:37:28 -0700

    Create a C function that fills a memory block with a constant byte.

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - a function that fills a memory  block with a constant byte.
+ * @s: pointer to the memory area to kick off
+ * @b: a variable to hold the const byte to be filled in
+ * @n: memory bytes to be filled starting from address at s
+ * Return: a pointer to the memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	if (n == 0)
+		return s;
+	*s = b;
+	_memset((s + 1), b, n - 1);
+}
+
+
+
