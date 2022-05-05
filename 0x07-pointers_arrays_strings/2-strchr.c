ec488db22f4151acd601245a3c7f4e017b8edd5e
Oluwasegun Emmanuel Ikoya <Oluwasegun Emmanuel Ikoya>
2022-05-05 10:49:23 -0700

    Create a C function to Check for a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strchr - a function that locates a character in a string.
+ * @s: pointer to string to be searched
+ * @c: character to be searched for
+ * Return: a pointer to the first occurrence of the character c
+ */
+
+char *_strchr(char *s, char c)
+{
+	if (*s == c)
+		return (s);
+	if (*s == '\0')
+		return (NULL);
+	_strchr(s + 1, c);
+}
