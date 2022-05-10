d480d6628c9b7e4953b3a1a66c7b686b7fb6ff25
sheggz <emmanuelikoya99@gmail.com>
2022-05-10 01:24:00 +0000

    Create a C function that allocates memory using malloc and a main to test

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/*
+ * malloc_checked - a function that allocates memory using malloc.
+ *		     function should cause normal process termination with a
+ *		      status value of 98
+ * @b: size needed memory
+ * Return: pointer to allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+		exit(98); /* terminate program with a code 98 */
+	return (p);
+}
