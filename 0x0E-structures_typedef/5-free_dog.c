e556f91d62dd7f48c5f699b271fa36d76a1e4f5a
sheggz <emmanuelikoya99@gmail.com>
2022-05-13 14:30:43 +0000

    Create a function to free memory soring a malloc-created struct
    the 5-main.c file is used to test this function. It is linked with
    other .c functions after compilation

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - a function thatg frees the space used to store a 'dog_t'
+ *	    struct that was created using 'new_dog'- a function that
+ *	    uses malloc to create a struct
+ * @d: pointer to memory used for the struct
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
