1386a88e704b5a290e71af30bd7f3523cec95540
sheggz <emmanuelikoya99@gmail.com>
2022-05-13 00:14:06 +0000

    Fix documentation Error

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,18 @@
+#ifndef GUARD
+#define GUARD
+/**
+ * struct dog - a datatype with attributes for a dog
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ */
+
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif
