3669e009c363b00c5799894e252fa44d1b3fe458
sheggz <emmanuelikoya99@gmail.com>
2022-05-13 11:08:27 +0000

    Create a C script to print the attributes of Struct

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - a function that prints the attributes of a dog obj type
+ * @d: pointer to struct
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	printf("Name: %s\n", d->name);
+	printf("Age: %lf\n", d->age);
+	printf("Owner: %s\n", d->owner);
+}
