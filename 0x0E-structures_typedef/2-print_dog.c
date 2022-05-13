e77cdecfa897065ac660450418b0f068200fd68d
sheggz <emmanuelikoya99@gmail.com>
2022-05-13 11:39:09 +0000

    Fix wrong output for some edge cases

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,6 +10,11 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+
 	printf("Name: %s\n", d->name);
 	printf("Age: %06f\n", d->age);
 	printf("Owner: %s\n", d->owner);
