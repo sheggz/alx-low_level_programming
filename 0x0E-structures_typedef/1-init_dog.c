81bd19e216217a072af90153305c8d35cbe5ec7c
sheggz <emmanuelikoya99@gmail.com>
2022-05-13 10:55:17 +0000

    Cover NULL entry edge case

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -5,13 +5,16 @@
  * init_dog - a function that initialize a variable of type struct dog
  * @d: pointer to the struct object to be initialized
  * @name: pointer to name ATTRIBUTE of the struct
- * @age: attribute 
+ * @age: attribute
  * @owner: attribute
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d -> name = name;
-	d -> age = age;
-	d -> owner = owner;
+	if (d == NULL)
+		return;
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
