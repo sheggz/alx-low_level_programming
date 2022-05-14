464fad72b401e8cfbd82b1f6ab0d4aed50311413
sheggz <emmanuelikoya99@gmail.com>
2022-05-14 00:49:56 +0000

    Create a function that prints a name usinf another function

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,11 +3,13 @@
 /**
  * print_name - a function that prints a name in a format determined by
  *		another funtion input
- * @name - pointer to name to be printed
- * @f - a pointer to a fuction that takes in a char pointer and returns nothing
+ * @name: pointer to name to be printed
+ * @f: a pointer to a fuction that takes in a char pointer and returns nothing
  */
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL)
+		return;
 	(*f)(name); /* defernce function pointer to access code and pass arg */
 }
