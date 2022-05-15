39aec011cc1843ea36118a2f29b88e9dbbd6bf1c
sheggz <emmanuelikoya99@gmail.com>
2022-05-15 14:37:09 +0000

    remove for loop from nain

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -13,33 +13,35 @@
 
 int main(int argc, char *argv[])
 {
-	int i, ans, arg_conv[2];
-	char *arr_sym[] = {"+", "-", "*", "/", "%"};
+	int ans, arg_conv[2];
+	/* char *arr_sym[] = {"+", "-", "*", "/", "%"}; */
 	int (*fnc_ptr)(int, int);
-	printf("%d\n", argc);
+
+	/* printf("%d\n", argc); - for debugging*/
 	if (argc != 4)
 	{
 		printf("Error");
 		exit(98);
 	}
 
-	for (i = 0; i < 5; i++)
-		if (strcmp(argv[2], arr_sym[i]) == 0)
+	/* for (i = 0; i < 5; i++) */
+	fnc_ptr = get_op_func(argv[2]);
+	printf("%p\n", fnc_ptr);
+	if (/* strcmp(argv[2], arr_sym[i]) == 0 */ fnc_ptr != NULL)
+	{
+		arg_conv[0] = atoi(argv[1]);
+		arg_conv[1] = atoi(argv[3]);
+
+		if (((strcmp(argv[2], "/") == 0) || (strcmp(argv[2], "%") == 0)) && arg_conv[1] == 0)
 		{
-			arg_conv[0] = atoi(argv[1]);
-			arg_conv[1] = atoi(argv[3]);
-
-			if (((strcmp(argv[2], "/") == 0) || (strcmp(argv[2], "%") == 0)) && arg_conv[1] == 0)
-			{
-				printf("Error");
-				exit(100);
-			}
-
-			fnc_ptr = get_op_func(argv[2]);
-			ans = (*fnc_ptr)(arg_conv[0], arg_conv[1]);
-			printf("%d\n", ans);
-			return (0);
+			printf("Error");
+			exit(100);
 		}
-	printf("Error");
+		/* fnc_ptr = get_op_func(argv[2]); */
+		ans = (*fnc_ptr)(arg_conv[0], arg_conv[1]);
+		printf("%d\n", ans);
+		return (0);
+	}
+	printf("Error\n");
 	exit(99);
 }
