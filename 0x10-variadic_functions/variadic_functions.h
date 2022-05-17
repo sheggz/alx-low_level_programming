34dd4d139e13cfb65c367750b44d5b585b0d870d
sheggz <emmanuelikoya99@gmail.com>
2022-05-17 02:05:04 +0000

    incorporate the Include guard in the header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,10 @@
+#ifndef GUARD
+#define GUARD
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 int _putchar(char c);
+
+#endif
