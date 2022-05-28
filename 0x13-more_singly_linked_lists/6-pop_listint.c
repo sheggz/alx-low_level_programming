71318a485bbda4ecca0fe019afba1d62f49e66a5
sheggz <emmanuelikoya99@gmail.com>
2022-05-28 04:58:20 +0000

    Create c functions to delete the head of a linked list

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,17 +1,17 @@
 #include "lists.h"
 
-/** 
+/**
  * pop_listint - a function that deletes the head node of a listint_t linked
- * 		 list
+ *		 list
  * @head: double poingterto the node head
  * Return: data at deleted node
- */ 
+ */
 
 int pop_listint(listint_t **head)
 {
 	listint_t *temp;
 	/*
-	 * to delete is to remove from the chain 
+	 * to delete is to remove from the chain
 	 * and the chain starts with a head pointer that points to the 1st node
 	 * SO WE WANT THE HEAD POINTER TO POINT TO THESECOND NODE
 	 */
