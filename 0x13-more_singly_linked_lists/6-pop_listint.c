07764aaa1d2eaaebdfa11daeed0e0958933e989b
sheggz <emmanuelikoya99@gmail.com>
2022-05-28 05:26:34 +0000

    Create a C function to delete the first node

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,16 +9,20 @@
 
 int pop_listint(listint_t **head)
 {
-	listint_t *temp;
+	int temp;
+	listint_t *tempp;
 	/*
-	 * to delete is to remove from the chain
+	 * to delete is to remove from the chain and free that memory location
 	 * and the chain starts with a head pointer that points to the 1st node
-	 * SO WE WANT THE HEAD POINTER TO POINT TO THESECOND NODE
+	 * SO WE WANT THE HEAD POINTER TO POINT TO THE SECOND NODE and that
+	 * memory location freed.
 	 */
+
 	if (*head == NULL)
 		return (0);
-	temp = *head;
-	*head = (*head)->next;
+	temp = (*head)->n;
+	tempp = (*head)->next;
 	free(*head);
-	return (temp->n);
+	*head = tempp;
+	return (temp);
 }
