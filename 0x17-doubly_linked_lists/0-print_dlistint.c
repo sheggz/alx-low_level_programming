62b708c9f9e25b69ba7b60d2e3069ba4fe4d03f2
sheggz <emmanuelikoya99@gmail.com>
2022-07-07 22:22:31 +0000

    [Create] C fn to get number of Nodes of a Dlikned list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_dlistint - a function that returns the number of nodes of
+ *		    a d-linked list
+ * @h: pointer to Node 1 (head pointer)
+ * Return: the number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	if (h != NULL)
+	{
+		return (1 + print_dlistint(h->next));
+	}
+	/* when we encounter NULL on forward traversal */
+	return (0);
+}
