a4611fb4aef66abcb80c1a5914c8d7d384041e72
sheggz <emmanuelikoya99@gmail.com>
2022-07-08 01:25:05 +0000

    [Create] C fn to add node at begining of D-linked list

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,20 +1,41 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include "lists.h"
+/**
+ * add_dnodeint - a fn to add a node at the begining of d-linked list
+ * @head: ptr variable that holds address of the head ptr
+ * @n: data to be stored in new node
+ * Return: address of new node
+ */
 
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint *new = malloc(sizeof(dlistint_t);
+	dlistint_t *new, *head_decoy;
+
+	if (!head)
+		return (NULL);
+	/* for easy understanding, save address of first node to a local var */
+	head_decoy = *head;
+
+	/* creat new node */
+	new = malloc(sizeof(dlistint_t));
 	if (!new)
 		return (NULL);
 	new->n = n;
 	new->prev = NULL;
 	new->next = NULL;
 
-	while (*head)
+	/* what happens if the head of the list is NULL */
+	if (!head_decoy)
 	{
-		head = &(*head->next);
+		*head = new;
+		return (*head);
 	}
-	*head->next = new;
-	new->prev = *head;
+	/* and if the head is not NULL? */
+
+	new->next = head_decoy;
+	head_decoy->prev = new;
+	*head = new;
+
+	return (new);
 }
