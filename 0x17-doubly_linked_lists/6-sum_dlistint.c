e2629209be84b14ff46dd59da3ab2e78f6bd403e
sheggz <emmanuelikoya99@gmail.com>
2022-07-08 03:16:40 +0000

    [Create] C fn to add data in all nodes

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ * sum_dlistint - fn to calculate the sum of data in a D-linked list
+ * @head: local variable aith address to node 1
+ * Return: sum of all data (n) in the nodes
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum;
+
+	sum = 0;
+
+	if (!head)
+		return (-1);
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	/* if index isnt found */
+	return (sum);
+}
+
