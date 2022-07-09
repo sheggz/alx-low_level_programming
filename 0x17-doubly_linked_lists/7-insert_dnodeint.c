67280890d4c8cb78d7ba7a4c03e4598a180c25cb
sheggz <emmanuelikoya99@gmail.com>
2022-07-09 04:03:47 +0000

    [Create] C function to add node in a d-linked list

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - a funcn to insert a node in a doubly linked list
+ *			      at a given index
+ * @idx: index of the node to be inserted
+ * @h: local variable to store the address of head
+ * @n: data to be stored in new node
+ * Return: address of new node or NULL on failure
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *head_decoy = *h, *temp, *new;
+	unsigned int i = 0;
+
+	if (!h)
+		return (NULL);
+	if (head_decoy == NULL || idx == 0)
+	{
+		*h = add_dnodeint(h, n);
+		if (*h == NULL)
+			return (NULL);
+		else
+			return (*h);
+	}
+	else
+	{
+		new = malloc(sizeof(dlistint_t));
+		if (!new)
+			return (NULL);
+		new->prev = NULL;
+		new->next = NULL;
+		new->n = n;
+
+		while (i < idx - 1)
+		{
+			h = &((*h)->next);
+			head_decoy = head_decoy->next;
+			i++;
+		}
+
+		temp = head_decoy->next;
+		head_decoy->next = new;
+		temp->prev = new;
+		new->next = temp;
+		new->prev = head_decoy;
+
+		return (new);
+
+	}
+
+}
