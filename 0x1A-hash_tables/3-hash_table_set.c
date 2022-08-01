5188ab41416055a1b7f7bddc4ed6c9fb78f37122
sheggz <emmanuelikoya99@gmail.com>
2022-08-01 13:38:50 +0000

    [Update] to update value incase of duplicate key

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -48,7 +48,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 				free(key_dupl);
 				free(temp->value);
 				temp->value = val_dupl;
-				return (0);
+				return (1);
 			}
 			temp = temp->next;
 		}
