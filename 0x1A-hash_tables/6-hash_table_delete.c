fbb09d235b1670df644d26b9dce23c84ebddd31a
sheggz <emmanuelikoya99@gmail.com>
2022-08-01 16:05:46 +0000

    [Create] a c function to delete a hashtable

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,16 +1,17 @@
 #include "hash_tables.h"
 
-/**
- *
- *
- */
 void free_list(hash_node_t *table);
+
+/**
+ * hash_table_delete - a function that deletes a hash table
+ * @ht: a pointer to the hash table to be deleted
+ */
 
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i = 0;
 
-	if (ht == NULL)
+	if (ht == NULL || ht->array == NULL)
 		return;
 
 	while (i < ht->size)
@@ -19,15 +20,24 @@ void hash_table_delete(hash_table_t *ht)
 			free_list(ht->array[i]);
 		i++;
 	}
+	/* when we're done freeing the lists, we free the array */
+	free(ht->array);
+	/* free the overall hashtable */
 	free(ht);
 }
 
-void free_list(hash_node_t *table)
+/**
+ * free_list - a recursive function to delete the nodes of a linked list
+ * @head: the head of the linked list to be freed
+ */
+
+void free_list(hash_node_t *head)
 {
-	if(table->next != NULL)
-		free_list(table->next);
-	free(table->key);
-	free(table->value);
-	free(table);
-	return;		
+	if (head->next != NULL)
+		free_list(head->next);
+	/* because we used strdup() to store the key and value, we free */
+	free(head->key);
+	free(head->value);
+	/* we free the node itsef */
+	free(head);
 }
